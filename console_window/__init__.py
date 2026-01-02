1acbe3a69e2134ca868148c7a388dee227bd2fad
Joe D <joedef@gmail.com>
2026-01-02 10:23:56 -0500

    more exposures

--- console_window/__init__.py
+++ console_window/__init__.py
@@ -3,10 +3,12 @@ from .ConsoleWindow import (
     ConsoleWindowOpts,
     OptionSpinner,
     IncrementalSearchBar,
+    InlineConfirmation,
     Theme,
     Context,
     Screen,
     ScreenStack,
     BasicHelpScreen,
     HOME_ST,
+    NAVIGATION_KEYS,
 )
